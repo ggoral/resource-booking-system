4c60642b3718df990ba2ee668d4857bf8d5a2ecd
ggoral <goral.gonzalo@gmail.com>
2014-02-11 18:21:06 -0300

    add json response

--- test/app_test.rb
+++ test/app_test.rb
@@ -7,9 +7,14 @@ class AppTest < Minitest::Test
     Sinatra::Application
   end
 
+  def assert_content_type_is_json(response)
+    content_type = response.headers['Content-Type']
+    assert_equal 'application/json;charset=utf-8', content_type
+  end
+
   def test_get_root
     get '/'
+    assert_content_type_is_json last_response
     assert_equal 200, last_response.status
-    assert_equal 'Hello World', last_response.body
   end
 end
