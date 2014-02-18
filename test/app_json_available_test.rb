5a4e30f3102b8054c02e3fc7261663b47ae0d04a
ggoral <goral.gonzalo@gmail.com>
2014-02-17 23:01:50 -0300

    remove commentary

--- test/app_json_available_test.rb
+++ test/app_json_available_test.rb
@@ -43,38 +43,4 @@ class AppJsonAvailabilityTest < Minitest::Unit::TestCase
     matcher = assert_json_match pattern, server_response.body  
   end
 
-  def test_json_bookings
-    server_response = get "/resources/#{@resource.id}/bookings/#{@booking.id}"
-    assert_equal 200, last_response.status
-    json = JSON.parse server_response.body
-    assert booking = json
-    
-    booking = @booking
-    pattern = {        
-      from: String,
-      to: String,
-      status: booking.status,
-      links:[
-        {  
-          rel: "self",
-          uri: String 
-        },
-        {  
-          rel: "resource",
-          uri: String
-        },
-        {  
-          rel: "accept",
-          uri: String,
-          method: "PUT"
-        },
-        {  
-          rel: "reject",
-          uri: String, 
-          method: "DELETE"
-        }
-      ]  
-    }
-    matcher = assert_json_match pattern, server_response.body  
-  end
 end
